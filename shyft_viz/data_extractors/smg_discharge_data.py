1e7c5d1ceef9db938d11453c5b977a7975a2c193
yisak <yisak.abdella@gmail.com>
2018-05-11 19:28:50 +0200

    added arg period to smg data extractor.

--- shyft_viz/data_extractors/smg_discharge_data.py
+++ shyft_viz/data_extractors/smg_discharge_data.py
@@ -9,12 +9,13 @@ class Geom(object):
 
 
 class SMGDataExtractor(object):
-    def __init__(self, shyft_data_ext, ref_repo, ref_ts):
+    def __init__(self, shyft_data_ext, ref_repo, ref_ts, period):
 
         self.var_units = {'q_avg': 'm3_per_sec'} # shyft_data_ext.var_units
         #self.t_ax = shyft_data_ext.t_ax
 
-        self.tsp = ref_repo.read([ts_info['uid'] for ts_info in ref_ts], shyft_data_ext.t_ax_shyft.total_period())
+        # self.tsp = ref_repo.read([ts_info['uid'] for ts_info in ref_ts], shyft_data_ext.t_ax_shyft.total_period())
+        self.tsp = ref_repo.read([ts_info['uid'] for ts_info in ref_ts], period)
         self.catch_names, self.ts_uid = zip(*[(ts_info['module_name'], ts_info['uid']) for ts_info in ref_ts])
         shyft_catch_names = shyft_data_ext.catch_names
         idx = [shyft_catch_names.index(c) for c in self.catch_names]
